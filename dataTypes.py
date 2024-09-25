bf34a57109e314206d8e64e3b121ecf778582d7f
Amit Raj <rajamitnine@gmail.com>
2024-09-25 17:33:31 +0530

    Add some in-built functions of numerical data in python like 'abs()', 'pow()', 'round()', 'min()', 'max()' and 'sum()'

--- dataTypes.py
+++ dataTypes.py
@@ -18,3 +18,22 @@ z = 5 + 9j
 print(z, type(z))
 print(z.real, type(z.real))
 print(z.imag, type(z.imag))
+
+# Some in-built method for numbers in python :
+
+# abs() method :
+print(abs(-5))
+
+# pow() method :
+power = pow(3, 4)
+print(power)
+
+# round() method :
+print(round(4.75))
+print(round(4.5))
+
+# min(), max() and sum() methods for list of numbers :
+nums = [4, 5, 1, 2, 8, 3, 6, 7]
+print(min(nums))
+print(max(nums))
+print(sum(nums))
