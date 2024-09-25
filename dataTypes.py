9aeb3e9a1a94db4d74b4e49190a01190abe3f090
Amit Raj <rajamitnine@gmail.com>
2024-09-25 16:38:01 +0530

    Add various/different numerical data types in python

---
+++ dataTypes.py
@@ -0,0 +1,20 @@
+# Different data types in python :
+
+# Numerical Data Types :
+
+# Integer :
+x = 5
+print(x, type(x))
+
+# float :
+y = 12.34
+print(y, type(y))
+
+num = 3e4
+print(num, type(num))
+
+# complex number :
+z = 5 + 9j
+print(z, type(z))
+print(z.real, type(z.real))
+print(z.imag, type(z.imag))
