a1e334123a720ffe554ec43f51ea151889f40151
Amit Raj <rajamitnine@gmail.com>
2024-09-25 15:55:21 +0530

    Add input() method to  take user Input

--- comments_input_output.py
+++ comments_input_output.py
@@ -1,7 +1,6 @@
 # This is a single line comment
 
 # DocString or multi line comment :
-
 '''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nisl urna, bibendum vel arcu quis, ultrices interdum neque. 
 Suspendisse ut fringilla mauris. Vivamus a aliquam ex, ut tempus erat. Nunc consectetur dui non orci tincidunt varius. 
@@ -9,3 +8,7 @@ Nam dignissim libero ut mi condimentum facilisis. Curabitur vestibulum enim augu
 ante id accumsan dapibus. Maecenas eu laoreet nunc, ut posuere tellus. Nulla id turpis a tortor faucibus auctor. Cras iaculis 
 tellus arcu, vel consectetur nulla commodo id. Phasellus eu tempus eros, nec blandit purus.
 '''
+
+# In-built input() method :
+name = input("Enter your fullname : ")
+print(name)
